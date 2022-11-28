5b217649655c379e4a4f72c68015d92f469028d9
VyacheslavZ <112565135+VyacheslavZ@users.noreply.github.com>
2022-11-28 23:43:17 +0800

    End.

---
+++ 2-1.py
@@ -0,0 +1,30 @@
+#Напишите программу, которая принимает на вход число N и выдает набор произведений (набор - это список) чисел от 1 до N.
+#Не используйте функцию math.factorial.
+#Пример:
+#- пусть N = 4, тогда [ 1, 2, 6, 24 ] #(1, 1*2, 1*2*3, 1*2*3*4)
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите Ваше число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
